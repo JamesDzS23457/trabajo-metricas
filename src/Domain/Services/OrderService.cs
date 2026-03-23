071c968c6d6d3aeaa4e1ee95cb7d368b2dcfa761
James David Amaya Bernal <jamayabe@uninpahu.edu.co>
2026-03-23 18:48:20 -0500

    refactorizacion de codigo y solucion de la advertencia S2223

--- src/Domain/Services/OrderService.cs
+++ src/Domain/Services/OrderService.cs
@@ -1,17 +1,23 @@
 using System;
 using System.Collections.Generic;
+using Domain.Entities;
 
 namespace Domain.Services;
 
-using Domain.Entities;
-
 public static class OrderService
 {
-    public static List<Order> LastOrders = new List<Order>();
+    public static List<Order> LastOrders { get; }= new List<Order>();
 
     public static Order CreateTerribleOrder(string customer, string product, int qty, decimal price)
     {
-        var o = new Order { Id = new Random().Next(1, 9999999), CustomerName = customer, ProductName = product, Quantity = qty, UnitPrice = price };
+        var o = new Order
+        {
+            Id = new Random().Next(1, 9999999), 
+            CustomerName = customer, 
+            ProductName = product, 
+            Quantity = qty, 
+            UnitPrice = price
+        };
         LastOrders.Add(o);
         return o;
     }
