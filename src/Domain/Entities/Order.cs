c8a52b93105895f6989537a279e62a4a1e735fa1
James David Amaya Bernal <jamayabe@uninpahu.edu.co>
2026-03-23 18:37:32 -0500

    Refactorizacion de get set

--- src/Domain/Entities/Order.cs
+++ src/Domain/Entities/Order.cs
@@ -1,19 +1,17 @@
-using System;
-using System.Collections.Generic;
+
 
 namespace Domain.Entities;
 
 public class Order
 {
-    public int Id;
-    public string CustomerName;
-    public string ProductName;
-    public int Quantity;
-    public decimal UnitPrice;
+    public int Id {get; set; }
+    public string CustomerName { get; set; }
+    public string ProductName { get; set; }
+    public int Quantity { get; init; }
+    public decimal UnitPrice { get; init; }
 
     public void CalculateTotalAndLog()
     {
         var total = Quantity * UnitPrice; 
-     
     }
 }
